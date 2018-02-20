f1e0f3b402bb60d25121a4fbca9fc5dfd057f66e
Grace Chiang <gracechiang@me.com>
2018-02-20 06:41:43 +0000

    example completed

---
+++ https_example.js
@@ -0,0 +1,23 @@
+var https = require("https");
+
+console.log("I did it!");
+
+var options = {
+    host: 'stream-large-file.herokuapp.com',
+    path: '/give-me-stuff-now'
+};
+
+var callback = function (response) {
+    console.log('In response handler callback!');
+
+    response.on('data', function (chunk) {
+        console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+        console.log(chunk.toString());
+    });
+}
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
